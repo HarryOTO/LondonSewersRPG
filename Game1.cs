b6c71db0b3a8073e8634875f7001fccd01e3e5f8
procharr <procharr@MHS-RM132-LT12>
2022-12-21 11:14:15 -0600

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,229 @@
+﻿using Cocos2D;
+using CocosDenshion;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using LondonSewersRPG.Source;
+
+namespace LondonSewersRPG
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        static Environment platforms;
+
+        static Texture2D charRat;
+        static Rectangle charRect;
+
+        static Texture2D buffRat;
+        static Rectangle buffRatRect;
+
+        static bool attackingState;
+        static bool falling;
+
+        static double gravity;
+        static double ySpeed;
+        static double xSpeed;
+        static double jumpSpeed;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            charRect = new Rectangle(355, 200, 90, 90);
+
+            falling = false;
+            attackingState = false;
+
+            gravity = .5;
+            ySpeed = 0;
+            xSpeed = 7;
+            jumpSpeed = -12;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            platforms = new Environment(
+                new int[,] {
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+                },
+                new int[,] {
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                    {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+                },
+                150, //startX
+                -310, //startY
+                100,
+                new Texture2D[] {
+                    this.Content.Load<Texture2D>("blank"),
+                    this.Content.Load<Texture2D>("wall"),
+                }
+            );
+
+            charRat = this.Content.Load<Texture2D>("charRat");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                ExitGame();
+            }
+
+            KeyboardState keys = Keyboard.GetState();
+
+            // falling starts when we a) jump, b) walk off an edge
+            // falling stops when we land on the ground from above
+            // gravity should be in effect
+            if (falling)
+            {
+                ySpeed += gravity;
+                platforms.move(0, -ySpeed);
+
+                bool intersected = false;
+                while (platforms.collides(charRect)[0] != -1)
+                {
+                    intersected = true;
+                    if (ySpeed < 0) // on the way up, hit our head
+                    {
+                        platforms.move(0, -1);
+                    }
+                    else if (ySpeed > 0) // on the way down, landed on ground
+                    {
+                        platforms.move(0, 1);
+                        falling = false;
+                    }
+
+                }
+                if (intersected)
+                {
+                    ySpeed = 0;
+                }
+
+            }
+            else
+            {
+                bool collided = false;
+                charRect.Y++;
+                if (platforms.collides(charRect)[0] != -1)
+                {
+                    collided = true;
+                }
+                charRect.Y--;
+
+                if (!collided)
+                {
+                    falling = true;
+                }
+
+                if (keys.IsKeyDown(Keys.W) || keys.IsKeyDown(Keys.Space))
+                {
+                    ySpeed = jumpSpeed;
+                    falling = true;
+                }
+
+            }
+
+            if (keys.IsKeyDown(Keys.A))
+            {
+                platforms.move(xSpeed, 0);
+                while (platforms.collides(charRect)[0] != -1)
+                {
+                    platforms.move(-1, 0);
+                }
+            }
+
+            if (keys.IsKeyDown(Keys.D))
+            {
+                platforms.move(-xSpeed, 0);
+                while (platforms.collides(charRect)[0] != -1)
+                {
+                    platforms.move(1, 0);
+                }
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(charRat, charRect, Color.White);
+            platforms.draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void ExitGame()
+        {
+            // TODO: add your exit code here to restore the device to its per-game environment.
+            CCSimpleAudioEngine.SharedEngine.RestoreMediaState();
+            Exit();
+        }
+    }
+}
---
+++ Source/Environment.cs
@@ -0,0 +1,127 @@
+﻿using Cocos2D;
+using CocosDenshion;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LondonSewersRPG.Source
+{
+    class Environment
+    {
+        public Rectangle[,] rectangles;
+        public double[,] actualXValues;
+        public double[,] actualYValues;
+        public int[,] collisionMap;
+        public int[,] textureMap;
+        public int startX;
+        public int startY;
+        public int size;
+        public Texture2D[] textures;
+
+        public Environment(
+            int[,] collisionMap,
+            int[,] textureMap,
+            int startX,
+            int startY,
+            int size,
+            Texture2D[] textures)
+        {
+
+            this.collisionMap = collisionMap;
+            this.textureMap = textureMap;
+            this.startX = startX;
+            this.startY = startY;
+            this.size = size;
+            this.textures = textures;
+            rectangles = new Rectangle[
+                this.textureMap.GetLength(0),
+                this.textureMap.GetLength(1)
+                ];
+
+
+            actualXValues = new double[
+                this.textureMap.GetLength(0),
+                this.textureMap.GetLength(1)
+                ];
+
+
+            actualYValues = new double[
+                this.textureMap.GetLength(0),
+                this.textureMap.GetLength(1)
+                ];
+
+            for (int row = 0; row < textureMap.GetLength(0); row++)
+            {
+                for (int col = 0; col < textureMap.GetLength(1); col++)
+                {
+                    rectangles[row, col] = new Rectangle(startX + size * col, startY + size * row, size, size);
+
+                    actualXValues[row, col] = rectangles[row, col].X;
+                    actualYValues[row, col] = rectangles[row, col].Y;
+                }
+            }
+        }
+
+
+        public int[] collides(Rectangle gameObject)
+        {
+            for (int row = 0; row < textureMap.GetLength(0); row++)
+            {
+                for (int col = 0; col < textureMap.GetLength(1); col++)
+                {
+                    if (gameObject.Intersects(rectangles[row, col]) && collisionMap[row, col] == 1)
+                    {
+                        return new int[] { row, col };
+                    }
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+
+        public void tp(int x, int y)
+        {
+            for (int row = 0; row < textureMap.GetLength(0); row++)
+            {
+                for (int col = 0; col < textureMap.GetLength(1); col++)
+                {
+                    rectangles[row, col].X = x + size * col;
+                    rectangles[row, col].Y = y + size * row;
+                }
+            }
+        }
+
+        public void move(double x, double y)
+        {
+            for (int row = 0; row < textureMap.GetLength(0); row++)
+            {
+                for (int col = 0; col < textureMap.GetLength(1); col++)
+                {
+                    actualXValues[row, col] += x;
+                    actualYValues[row, col] += y;
+                    rectangles[row, col].X = (int)actualXValues[row, col];
+                    rectangles[row, col].Y = (int)actualYValues[row, col];
+                }
+            }
+        }
+
+
+        public virtual void draw(SpriteBatch spriteBatch)
+        {
+            for (int row = 0; row < textureMap.GetLength(0); row++)
+            {
+                for (int col = 0; col < textureMap.GetLength(1); col++)
+                {
+                    spriteBatch.Draw(
+                        textures[textureMap[row, col]],
+                        rectangles[row, col],
+                        Color.White
+                        );
+                }
+            }
+        }
+
+
+
+    }
+}
+
